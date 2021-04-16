8e31bd78eccbddf2dd1a437c70d19bf536a313ee
Fastus001 <tkarczmarzyk@gmail.com>
2021-04-16 17:21:34 +0200

    175 - redirects and wildcards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MoviesComponent} from './pages/movies/movies.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
 
 const  routes: Routes = [
-  {path: 'movies', component: MoviesComponent}
+  {path: '', redirectTo: '/movies', pathMatch: 'full'},
+  {path: 'movies', component: MoviesComponent},
+  {path: '**', component: PageNotFoundComponent},
 ]
 
 @NgModule({

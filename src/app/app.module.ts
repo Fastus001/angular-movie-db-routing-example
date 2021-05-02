dbf2be1fdf812ee0b92dd5b3850e99e14dabfaf3
Fastus001 <tkarczmarzyk@gmail.com>
2021-05-02 13:34:49 +0200

    196 - preparing template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { MovieCoverComponent } from './shared/movie-cover/movie-cover.component';
-import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
-import { MoviesComponent } from './pages/movies/movies.component';
-import { CategoriesComponent } from './pages/categories/categories.component';
-import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
-import { AppRoutingModule } from './app-routing.module';
-import { YearsComponent } from './pages/years/years.component';
-import { MovieInYearComponent } from './pages/years/movie-in-year/movie-in-year.component';
-import { HttpTestComponent } from './pages/http-test/http-test.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {MovieCoverComponent} from './shared/movie-cover/movie-cover.component';
+import {MovieDetailsComponent} from './pages/movies/movie-details/movie-details.component';
+import {MoviesComponent} from './pages/movies/movies.component';
+import {CategoriesComponent} from './pages/categories/categories.component';
+import {MoviesInCategoryComponent} from './pages/categories/movies-in-category/movies-in-category.component';
+import {AppRoutingModule} from './app-routing.module';
+import {YearsComponent} from './pages/years/years.component';
+import {MovieInYearComponent} from './pages/years/movie-in-year/movie-in-year.component';
+import {HttpTestComponent} from './pages/http-test/http-test.component';
+import {AddMovieComponent} from './pages/add-movie/add-movie.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,8 +29,9 @@ import { HttpTestComponent } from './pages/http-test/http-test.component';
     YearsComponent,
     MovieInYearComponent,
     HttpTestComponent,
+    AddMovieComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
